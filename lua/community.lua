b9d1749a4456dc6758d7256d988c513cd126ab13
davey <davidjharned@gmail.com>
2025-02-16 23:42:10 -0800

    Updated theme!

--- lua/community.lua
+++ lua/community.lua
@@ -7,5 +7,6 @@ return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.cs-omnisharp" },
+  { import = "astrocommunity.colorscheme.catppuccin" },
   -- import/override with your plugins folder
 }
