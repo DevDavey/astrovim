76b625999b57b09f69e26917ca42b385a5a81a30
DeveloperDavey <davidjharned@gmail.com>
2025-02-16 21:52:11 -0800

    initial commit of AstroVim Config

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,21 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = false,
+          hide_dotfiles = true,
+          hide_gitignore = true,
+          hide_by_name = {
+            ".DS_Store",
+            "thumbs.db",
+            "node_modules",
+            "__pycache__",
+            "*.meta",
+          },
+        },
+      },
+    },
+  },
+}
